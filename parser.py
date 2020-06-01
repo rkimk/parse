50a7f2490c72258cdc900aff4fb18cedb70a41a0
rkimk <admin@rkimk.ru>
2020-06-02 02:20:32 +0300

    Worked

--- parser.py
+++ parser.py
@@ -1,22 +1,20 @@
 import fake_useragent
-import requests, os.path
+import requests, os.path, re
 from bs4 import BeautifulSoup as bs
-import browsercookie
+
 ua = fake_useragent.UserAgent()
-# ua.update()
 user = ua.random
-
+session = requests.Session()
+useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
 header = {'User-Agent': str(user)}
-# proxies = {
-#     'http': 'socks5://192.168.1.3:9100',
-#     'https': 'socks5://192.168.1.3:9100',
-# }
-
 proxies = {
-    'http': 'socks5://171.112.94.9:38801',
-    'https': 'socks5://171.112.94.9:38801',
+    'http': 'socks5://192.168.1.3:9100',
+    'https': 'socks5://192.168.1.3:9100',
 }
-
+# proxies = {
+#     'http': 'socks5://171.112.94.9:38801',
+#     'https': 'socks5://171.112.94.9:38801',
+# }
 line = "---------------------------------------------------------------"
 ipSite = 'http://icanhazip.com'
 
@@ -46,76 +44,56 @@ load = {
     'submit': ''}
 
 
-
-def get_link():
+def get_link(single_url):
     with open(f'page{single_url[-7:]}.html', 'r', encoding='utf8') as file:
-        link = bs(file, 'lxml')
-        # print(link)
-        ta = link.find_all('a')
-        for a in ta:
+        links = bs(file, 'lxml')
+        pattern = 'Скачать'
+        link = links.find_all('a', text=pattern, attrs={'rel': 'nofollow'})
+        for a in link:
             print(a)
-        # [el.extract() for el in link.select("[style*='border-spacing: 2px;']")]
-        # print(link.prettify())
-        # print(ta)
 
 
-        # for b in ta:
-        #     print(b)
-        # ta = link.find_all('td')
-        # for j in ta:
-        #     links = j.find('a').get('href')
-        # print(links)
-        # print(ta)
-        # for b in link:
-        #     tdd = b.find('href')
-        #     print(tdd)
+# get_link('http://nnmclub.to/forum/viewtopic.php?t=1379359')
 
-# get_link()
-
-# def getfile(single_url, header, proxies, ipSite, load):
-#
-#     adress = requests.get(ipSite, headers=header, proxies=proxies)
-#     print(header)
-#     print(line + "\n[*] IP your network:\n" + adress.text + line)
-#     if os.path.exists(f'page{single_url[-7:]}.html'):
-#         os.remove(f'page{single_url[-7:]}.html')
-#     single_film = session.post(url, headers=header, proxies=proxies, data=load)
-#     page = bs(single_film.text, 'lxml')
-#     with open(f'page{single_url[-7:]}.html', 'w', encoding='utf8') as f:
-#         f.writelines(str(page))
+def getfile(single_url, header, proxies):
+    # if os.path.exists(f'page{single_url[-7:]}.html'):
+    #     os.remove(f'page{single_url[-7:]}.html')
+    single_film = session.get(single_url, headers=header, proxies=proxies)
+    page = bs(single_film.text, 'lxml')
+    pattern = 'Скачать'
+    link = page.find_all('a', text=pattern, attrs={'rel': 'nofollow'})
+    for a in link:
+        # print(str(a['href']))
+        download_link = f'http://nnmclub.to/forum/{str(a["href"])}'
+        print('Download_link:' + ' ' + download_link + '\n')
+    # with open(f'page{single_url[-7:]}.html', 'w', encoding='utf8') as f:
+    #     f.writelines(str(page))
+    # get_link(single_url)
 
 
 def getfilms(url, header, proxies, load):
+    adress = requests.get(ipSite, headers=header, proxies=proxies)
+    print(header)
+    print(line + "\n[*] IP your network:\n" + adress.text + line)
     if os.path.exists('nmclub.html'):
         os.remove('nmclub.html')
-    with requests.Session() as session:
-        content = session.post(url, headers=header, proxies=proxies, data=load)
-        cookies = content.cookies
+    content = session.post(url, headers=header, proxies=proxies, data=load)
+    soup = bs(content.text, 'lxml')
+    films = []
+    find_class_td = soup.find_all('td', attrs={'class': 'genmed'})
+    for j in find_class_td:
+        link = j.find('a').get('href')
+        films.append({
+            j.text: link
+        })
+        print('Title:' + ' ' + j.text + ' ' + 'Link:' + ' ' + link)
+        with open('nmclub.html', 'a', encoding='utf-8') as file:
+            file.writelines('\n' + str(j))
+        single_url = f"http://nnmclub.to/forum/{link}"
+        print(single_url)
+        getfile(single_url, header, proxies)
 
-        print(cookies)
-        soup = bs(content.text, 'lxml')
-        films = []
-        find_class_td = soup.find_all('td', attrs={'class': 'genmed'})
 
-        for j in find_class_td:
-            link = j.find('a').get('href')
-            films.append({
-                j.text: link
-            })
 
-        # print('Title:' + ' ' + j.text + ' ' + 'Link:' + ' ' + link)
-
-            with open('nmclub.html', 'a', encoding='utf-8') as file:
-                file.writelines('\n' + str(j))
-            single_url = f"http://nnmclub.to/forum/{link}"
-    # print(films)
-
-        # getfile(single_url, header, proxies, ipSite, load)
-            print(single_url)
-            single_film = session.get(url, headers=header, proxies=proxies, data=load)
-            page = bs(single_film.text, 'lxml')
-            with open(f'page{single_url[-7:]}.html', 'w', encoding='utf8') as f:
-             f.writelines(str(page))
-            break
 
 getfilms(url, header, proxies, load)
