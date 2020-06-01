16192cd9415349876ec7b46b160e21ecd7aacecb
rkimk <admin@rkimk.ru>
2020-06-01 15:40:53 +0300

    lxml

--- parser.py
+++ parser.py
@@ -1,26 +1,29 @@
 import urllib.request
-import requests
+
 from bs4 import BeautifulSoup as bs
 import requests, fake_useragent
 
 ua = fake_useragent.UserAgent()
 user = ua.random
 headers = {'User-Agent': str(user)}
-base_url = 'https://www.dropbox.com/s/xedukyjtbpf432p/schedfile.json?dl=0'
+base_url = 'http://nnmclub.to/'
+proxies = {'http': 'socks5://192.168.1.3:9050', 'https': 'socks5://192.168.1.3:9050'}
 
 
 def drop_parse(base_url, headers):
-    jobs = []
-    session = requests.session()
-    request = session.get(base_url, headers=headers)
-    if request.status_code == 200:
-        soup = bs(request.content, 'lxml')
+
+
+
+
+    req = requests.get(base_url, headers=headers, proxies=proxies)
+    if req.status_code == 200:
+        soup = bs(req.content, 'lxml')
         divs = soup.find_all('div')
         for div in divs:
             print(div)
-        with open('file.html', 'w') as f:
+        with open('file.html', 'w', encoding='utf8') as f:
             f.write(str(divs))
-    # print(divs)
+    print(divs)
 
 
 drop_parse(base_url, headers)
