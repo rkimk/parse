bc7b6e9d218fcd8bd2bc02696e34c25d4885a157
rkimk <admin@rkimk.ru>
2020-06-02 15:43:46 +0300

    Change filesize

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 import fake_useragent
-import requests, os.path
+import requests, os.path, re
 from bs4 import BeautifulSoup as bs
 
 ua = fake_useragent.UserAgent()
@@ -43,14 +43,38 @@ load = {
     'submit': ''}
 
 
-def getfile(single_url, header, proxies):
+def getfile(download_link, name):
+    file = session.get(download_link, proxies=proxies)
+    name = name.replace('/', '_')
+    name = name.replace(':', '-')
+    dir = os.path.abspath('downloads')
+    with open(f'{dir}/{name}.torrent', 'wb') as f:
+        f.write(file.content)
+
+
+def getlink(single_url, header, proxies, name):
     single_film = session.get(single_url, headers=header, proxies=proxies)
     page = bs(single_film.text, 'lxml')
     pattern = 'Скачать'
     link = page.find_all('a', text=pattern, attrs={'rel': 'nofollow'})
+
     for a in link:
         download_link = f'http://nnmclub.to/forum/{str(a["href"])}'
-        print('Download_link:' + ' ' + download_link + '\n')
+        print('Download_link:' + ' ' + download_link)
+
+        regex = r"\d+,"
+        for i in range(0, 18, 2):
+            # print(i)
+            size = page.find_all('span', attrs={'title': f'Размер блока: {i} MB'})
+            for b in size:
+                print("Размер торрента составляет: " + re.search(regex, b.get_text()).group(0) + '\n')
+                try:
+                    filesize = int(re.search(regex, b.get_text()).group(0)[:-1])
+                    if filesize <= 15:
+                        getfile(download_link, name)
+
+                except AttributeError:
+                    print('Not a file')
 
 
 def getfilms(url, header, proxies, load):
@@ -64,6 +88,7 @@ def getfilms(url, header, proxies, load):
     films = []
     find_class_td = soup.find_all('td', attrs={'class': 'genmed'})
     for j in find_class_td:
+        name = j.text
         link = j.find('a').get('href')
         films.append({
             j.text: link
@@ -73,7 +98,7 @@ def getfilms(url, header, proxies, load):
             file.writelines('\n' + str(j))
         single_url = f"http://nnmclub.to/forum/{link}"
         print(single_url)
-        getfile(single_url, header, proxies)
+        getlink(single_url, header, proxies, name)
 
 
 getfilms(url, header, proxies, load)
