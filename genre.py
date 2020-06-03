02b42031bc2f9a4037b4457341939a4e45388b00
rkimk <admin@rkimk.ru>
2020-06-03 08:12:29 +0300

    Correct genre

--- genre.py
+++ genre.py
@@ -3,6 +3,7 @@ with open('file.html') as file:
     page = bs(file, 'lxml')
     find_div = page.find_all('div', attrs={'class':'postbody'})
     find_span = find_div[0].find_all('span', attrs={'style': 'font-weight: bold'})
-    startpos = str(find_div[0]).find(f'{find_span[2]}') + len(f'{find_span[2]}')
+    span = '<span style="font-weight: bold">Жанр:</span>'
+    startpos = str(find_div[0]).find(span) + len(span)
     endpos = str(find_div[0]).find('<br/><br/>', startpos)
     print(str(find_div[0])[startpos:endpos].strip())
--- parser.py
+++ parser.py
@@ -63,8 +63,9 @@ def getfile(download_link, name):
 def genre(page):
     soup = bs(page.text, 'lxml')
     find_div = soup.find_all('div', attrs={'class': 'postbody'})
-    find_span = find_div[0].find_all('span', attrs={'style': 'font-weight: bold'})
-    startpos = str(find_div[0]).find(f'{find_span[2]}') + len(f'{find_span[2]}')
+    # find_span = find_div[0].find_all('span', attrs={'style': 'font-weight: bold'})
+    span = '<span style="font-weight: bold">Жанр:</span>'
+    startpos = str(find_div[0]).find(span) + len(span)
     endpos = str(find_div[0]).find('<br/><br/>', startpos)
     genre_string = str(find_div[0])[startpos:endpos].strip()
     # print(str(find_div[0])[startpos:endpos].strip())
